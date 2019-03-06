4376c188d7f15505109fdbfa5dbdd2c5cfc720b3
kendrahavens <kehavens@microsoft.com>
2019-03-05 17:36:46 -0800

    initial commit

---
+++ UserStudy0307/Class1.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Class1
+{
+
+    public void Method1()
+    {
+        //Copy the code below
+        List<string> greetings = new List<string>()
+            { "hi", "yo", "hello", "howdy" };
+        string json = JsonConvert.SerializeObject("some json");
+        StringBuilder sb = new StringBuilder("ABC", 50);
+    }
+    public void Method2(int a, int b, int c, int d, string e, string f, string g, string h, DateTime i, DateTime j, DateTime k)
+    {
+    }
+
+    public void Method3()
+    {
+        string HelloWorld()
+        {
+            return "HelloWorld!";
+        }
+        Console.WriteLine(HelloWorld());
+
+    }
+
+    public int Method4()
+    {
+        int x = 1;
+        x = 2;
+        return x;
+    }
+
+    public void Method5()
+    {
+        var greetings = new List<string>()
+            { "hi", "yo", "hello", "howdy" };
+
+        IEnumerable<string> enumerable()
+        {
+            foreach(var greet in greetings)
+            {
+                if(greet.Length < 3)
+                {
+                    yield return greet;
+                }
+            }
+
+            yield break;
+        }
+
+        var shortGreeting = enumerable();
+    }
+
+}
+
+public interface MyInterface
+{
+}
+public class MyClass : MyInterface
+{
+    public void MyMethod()
+    {
+        System.Console.WriteLine("Hello World");
+    }
+
+    public string MyString { get; set; }
+    public int MyInt { get; set; }
+    public DateTime MyDateTime { get; set; }
+    public bool MyBool { get; set; }
+}
---
+++ UserStudy0307/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserStudy0307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Paste code here
+
+
+        }
+    }
+
+}
